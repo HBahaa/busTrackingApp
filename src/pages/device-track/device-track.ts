e3cb577d932f147d3d0c3a57d5bd0f98ba85838a
HBahaa <hebabahaa2208@yahoo.com>
2018-09-15 09:17:18 +0200

    add actions to device track

--- src/pages/device-track/device-track.ts
+++ src/pages/device-track/device-track.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheetController } from 'ionic-angular';
 import { GoogleMaps, GoogleMap, GoogleMapsEvent, GoogleMapOptions } from '@ionic-native/google-maps';
 
-declare var google;
 
 @Component({
   selector: 'page-device-track',
@@ -13,7 +12,7 @@ export class DeviceTrackPage {
   lat: any;
   lng: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public actionsheetCtrl: ActionSheetController) {
   }
 
   ionViewDidLoad() {
@@ -34,9 +33,6 @@ export class DeviceTrackPage {
     this.map = GoogleMaps.create('map', mapOptions);    
     this.map.one(GoogleMapsEvent.MAP_READY)
       .then(() => {
-          var infoWindow = new google.maps.InfoWindow({
-          content: contentString
-        });
         this.map.addMarker({
           title: 'Location',
           icon: 'red',
@@ -49,29 +45,59 @@ export class DeviceTrackPage {
         .then(marker => {
           marker.on(GoogleMapsEvent.MARKER_CLICK)
             .subscribe(() => {
-              infoWindow.open(marker);
+              alert("clicked")
             });
         });
 
       });
 
-      var contentString = `
-        <ion-fab>
-          <button ion-fab color="danger"><ion-icon name="md-share"></ion-icon></button>
-          <ion-fab-list side="top">
-            <button ion-fab color="primary"><ion-icon name="logo-vimeo"></ion-icon></button>
-          </ion-fab-list>
-          <ion-fab-list side="bottom">
-            <button ion-fab color="secondary"><ion-icon name="logo-facebook"></ion-icon></button>
-          </ion-fab-list>
-          <ion-fab-list side="left">
-            <button ion-fab color="light"><ion-icon name="logo-googleplus"></ion-icon></button>
-          </ion-fab-list>
-          <ion-fab-list side="right">
-            <button ion-fab color="dark"><ion-icon name="logo-twitter"></ion-icon></button>
-          </ion-fab-list>
-        </ion-fab>
-      `;
+     
+  }
+
+  openMenu() {
+    let actionSheet = this.actionsheetCtrl.create({
+      title: 'Actions',
+      buttons: [
+        {
+          text: 'Information',
+          //role: 'destructive',
+          icon: 'information',
+          handler: () => {
+            alert('information clicked');
+          }
+        },
+        {
+          text: 'Pause',
+          icon: 'pause',
+          handler: () => {
+            alert('pause clicked');
+          }
+        },
+        {
+          text: 'Play',
+          icon: 'play',
+          handler: () => {
+            alert('Play clicked');
+          }
+        },
+        {
+          text: 'Help',
+          icon: 'help',
+          handler: () => {
+            alert('help clicked');
+          }
+        },
+        {
+          text: 'Cancel',
+          //role: 'cancel', // will always sort to be on the bottom
+          icon: 'close',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
 
 }
